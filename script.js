2c3b73b1f7e765a617b738505a1347257fbf5ec7
Chameleon <chameleon@Xubuntu.myguest.virtualbox.org>
2023-09-14 16:17:38 +0300

    Added graphical elements, bugfixes(bookshelf-content, viewport)

--- script.js
+++ script.js
@@ -3,54 +3,58 @@ const myLibrary = [
         title: "The Clean Coder",
         author: "Robert C. Martin",
         pages: 244,
-        status: "not read"
+        status: "not-read"
     },
     {
         title: "Don't Make Me Think",
         author: "Steve Krug",
         pages: 209,
-        status: "not read"
+        status: "not-read"
     },
     {
         title: "Dive Into Design Patterns",
         author: "Alexander Shvets",
         pages: 410,
-        status: "not read"  
+        status: "not-read"  
     },
     {
         title: "Eloquent JavaScript",
         author: "Marijn Haverbeke",
         pages: 447,
-        status: "currently reading"
+        status: "currently-reading"
     },
     {
         title: "C++ Primer Plus",
         author: "Stephen Prata",
         pages: 1438,
-        status: "not read"
+        status: "not-read"
     },
     {
         title: "Web Typography",
         author: "Richard Rutter",
         pages: 201,
-        status: "not read"
+        status: "not-read"
     },
     {
         title: "Articulating Design Decisions",
         author: "Tom Greever",
         pages: 275,
-        status: "not read"
+        status: "not-read"
     },
     {
         title: "Think Like a Programmer",
         author: "V. Anton Spraul",
         pages: 256,
-        status: "currently reading"
+        status: "currently-reading"
     },
- 
+    {
+        title: "DOM Enlightenment",
+        author: "Cody Lindley",
+        pages: 178,
+        status: "currently-reading"
+    }
 
 ];
-// console.log(myLibrary);
 
 function Book(title, author, pages, status) {
     this.title = title;
@@ -88,10 +92,10 @@ function displayBooks() {
   
     myLibrary.forEach((book, index) => {
         const listItem = document.createElement('li');
-        const statusText = capitalize(book.status); 
+        const statusText = book.status.toUpperCase();
         listItem.innerHTML = `
           <div class="book-item">
-            <span>${book.title} by ${book.author}, ${book.pages} pages, ${statusText}</span>
+            <span><i>${book.title}</i> by <b>${book.author}</b>, ${book.pages} pages, ${statusText}</span>
             <button onclick="removeBook(${index})">Remove</button>
           </div>
         `;
@@ -108,5 +112,5 @@ function capitalize(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
-document.querySelector('input[name="status"][value="not-read"]').checked = true;
+document.querySelector('input[name="status"][value="read"]').checked = true;
 displayBooks();
